4d68c7d7bbbe1db7b144c6ed35c454c30996411c
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2024-10-29 00:36:48 -0300

    att

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoButtonDeleteAllComponent } from '../todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItensComponent } from '../todo-input-add-itens/todo-input-add-itens.component';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [TodoInputAddItensComponent, TodoButtonDeleteAllComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
